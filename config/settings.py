bb4dae25247ddecd7ee6ae6a73a86b162b9e5706
sasha <sashapenkin12@mail.ru>
2025-11-20 20:23:13 +0600

    feat(deploy): update

--- config/settings.py
+++ config/settings.py
@@ -136,6 +136,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://photoeasy.duckdns.org',
+]
+
+CORS_TRUSTED_ORIGIN = [
+    'https://easy-photo.vercel.app',
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
